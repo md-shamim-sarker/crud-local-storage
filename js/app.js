4a064738622733982a9e1f50a85f6bbce7b4e026
shamimspro <shamimspro@gmail.com>
2022-09-06 23:48:23 +0600

    design with bootstrap

--- js/app.js
+++ js/app.js
@@ -9,12 +9,16 @@ const personsArray = JSON.parse(getPersons);
 document.getElementById('btn-submit').addEventListener('click', () => {
     const slValue = personsArray.length + 1;
     const nameValue = document.getElementById('name-field').value;
-    const ageValue = Number(document.getElementById('age-field').value);
-    personsArray.push({sl: slValue, name: nameValue, age: ageValue});
-    localStorage.setItem('persons', JSON.stringify(personsArray));
-    display();
-    document.getElementById('name-field').value = '';
-    document.getElementById('age-field').value = '';
+    const ageValue = document.getElementById('age-field').value;
+    if(!nameValue || !ageValue) {
+        alert('Please Enter Name & Age!');
+    } else {
+        personsArray.push({sl: slValue, name: nameValue, age: ageValue});
+        localStorage.setItem('persons', JSON.stringify(personsArray));
+        display();
+        document.getElementById('name-field').value = '';
+        document.getElementById('age-field').value = '';
+    }
 });
 
 const display = () => {
@@ -26,8 +30,8 @@ const display = () => {
             <td>${personsArray.indexOf(element) + 1}</td>
             <td>${element.name}</td>
             <td>${element.age}</td>
-            <td><button onclick="updateItem(this.value)" value="${element.name}">Update</button></td>
-            <td><button onclick="removeItem(this.value)" value="${element.name}">Delete</button></td>
+            <td><button class="btn btn-success" onclick="updateItem(this.value)" value="${element.name}">Update</button></td>
+            <td><button class="btn btn-danger" onclick="removeItem(this.value)" value="${element.name}">Delete</button></td>
         `;
         tbody.appendChild(tr);
     });
